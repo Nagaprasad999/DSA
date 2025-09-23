e1cb6152bfe829343ff8281806991bd203084eb2
Nagaprasad Mahipathi <168118059+Nagaprasad999@users.noreply.github.com>
2025-09-23 22:35:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.java
+++ Difficulty: Medium/Next Permutation/next-permutation.java
@@ -1,47 +1,44 @@
 class Solution {
+     public static void swap(int arr[],int start,int end){
+           
+           int temp   = arr[start];
+           arr[start]=arr[end];
+           arr[end]=temp;
+           
+       }
+       public static void reverse(int arr[],int start,int end){
+           while(start<end){
+               swap(arr,start,end);
+               start++;
+               end--;
+           }
+       }
     int[] nextPermutation(int[] arr) {
-        int n = arr.length;
-        int ind = -1;
-
-        // Step 1: Find the pivot index
-        for (int i = n - 2; i >= 0; i--) {
-            if (arr[i] < arr[i + 1]) {
-                ind = i;
-                break;
-            }
-        }
-
-        // Step 2: If no pivot found, reverse whole array (last permutation case)
-        if (ind == -1) {
-            reverse(arr, 0, n - 1);
-            return arr;
-        }
-
-        // Step 3: Find element just greater than pivot
-        for (int i = n - 1; i > ind; i--) {
-            if (arr[i] > arr[ind]) {
-                swap(arr, i, ind);
-                break;
-            }
-        }
-
-        // Step 4: Reverse suffix after pivot
-        reverse(arr, ind + 1, n - 1);
-
-        return arr;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    private void reverse(int[] arr, int start, int end) {
-        while (start < end) {
-            swap(arr, start, end);
-            start++;
-            end--;
-        }
-    }
+       int n=arr.length;
+       
+       int idx=-1;
+       for(int i=n-2;i>=0;i--){
+           if(arr[i]<arr[i+1]){
+               idx=i;
+               break;
+           }
+       }
+       if(idx==-1){
+           reverse(arr,0,n-1);
+           return arr;
+       }
+      
+       for(int i=n-1;i>=0;i--){
+           if(arr[i]>arr[idx]){
+               swap(arr,idx,i);
+         
+               break;
+           }
+       }
+       reverse(arr,idx+1,n-1);
+       
+      return arr;
+     
+   }
 }
+    
