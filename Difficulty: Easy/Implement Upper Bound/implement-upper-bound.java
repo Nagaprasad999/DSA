2ab04c787b9771c923450097eeb18b0ab5f31a05
Nagaprasad Mahipathi <168118059+Nagaprasad999@users.noreply.github.com>
2025-10-05 19:59:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
@@ -0,0 +1,22 @@
+class Solution {
+    int upperBound(int[] arr, int x) {
+        // code here
+           int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = n; // Default: if not found, insertion point is at end
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;        // possible position
+                high = mid - 1;   // look on the left for smaller index
+            } else {
+                low = mid + 1;    // look on the right side
+            }
+        }
+
+        return ans;
+    }
+}
