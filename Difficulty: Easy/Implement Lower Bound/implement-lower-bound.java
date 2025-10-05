146b085aea2e6e98c558857a71730a7f14a14d08
Nagaprasad Mahipathi <168118059+Nagaprasad999@users.noreply.github.com>
2025-10-05 21:17:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -1,21 +1,20 @@
 class Solution {
     public int lowerBound(int[] arr, int x) {
-        int n = arr.length;
-        int low = 0;
-        int high = n - 1;
-        int ans = n; // Default: if not found, insertion point is at end
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (arr[mid] >= x) {
-                ans = mid;        // possible position
-                high = mid - 1;   // look on the left for smaller index
-            } else {
-                low = mid + 1;    // look on the right side
-            }
+        
+      int n=arr.length;
+      int low=0;
+      int high=n-1;
+      int result=arr.length;
+     while(low<=high){
+         
+         int mid=(low+high)/2;
+        if(arr[mid]>=x){
+            result=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
         }
-
-        return ans;
+      }
+      return result;
     }
 }
